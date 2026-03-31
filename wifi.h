237157a692f8469b91035d6a0e9495297ee306db
RubenBuysschaertVives <ruben.buysschaert@vives.be>
2026-03-31 09:44:09 +0200

    Added WiFi-module files.

---
+++ wifi.h
@@ -0,0 +1,16 @@
+#if !defined(WIFI_DEFINED)
+	#define WIFI_DEFINED
+	
+	// TODO: change here according to personal whishes/settings.	
+	#define WIFI_SSID 			"A118_IWT_TEMPORARY"
+	#define WIFI_PWD 				"VIVES_ELOICT"
+
+	
+	#define MQTT_SUB_TOPIC 				".../leds"
+	#define MQTT_PUB_TOPIC 				".../buttonsadleds"
+	#define MQTT_BROKER_ADDRESS 	"mqtt.rubu.be"
+	#define MQTT_PORT 						1885
+	#define MQTT_USER 						"..."
+	#define MQTT_PWD 							"..."
+	#define MQTT_CLIENT_ID 				"ESP8266_client_id_..."
+#endif
