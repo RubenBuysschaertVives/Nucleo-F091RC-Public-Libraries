6c3c87cb3495b5480e0ebb2d59637d9115b3d5fd
RubenBuysschaertVives <ruben.buysschaert@vives.be>
2026-06-30 11:34:57 +0200

    Added new functions.

--- apa102c.c
+++ apa102c.c
@@ -2,6 +2,8 @@
 #include "apa102c.h"
 #include "spi1.h"
 
+APA102C leds[NUMBER_OF_APA102C_LEDS];
+
 // Verstuur de data over SPI1. Let op: zorg dat SPI1 reeds geïnitialiseerd is.
 void UpdateAPA102CLeds(APA102C led[])
 {
@@ -24,4 +26,32 @@ void UpdateAPA102CLeds(APA102C led[])
 	ByteToSpi1(255);
 	ByteToSpi1(255);
 	ByteToSpi1(255);
+}
+
+void ClearAPA102CLeds(APA102C leds[])
+{
+	for(uint8_t i = 0; i < NUMBER_OF_APA102C_LEDS; i++)
+	{
+		leds[i].brightness = DEFAULT_LED_BRIGHTNESS;
+		leds[i].red = 0;
+		leds[i].green = 0;
+		leds[i].blue = 0;		
+	}
+	
+	UpdateAPA102CLeds(leds);
+}
+
+void SetAPA102CLed(APA102C leds[], uint8_t ledNumber, uint8_t red, uint8_t green, uint8_t blue)
+{
+	ClearAPA102CLeds(leds);
+	
+	if(ledNumber < NUMBER_OF_APA102C_LEDS)
+	{
+		leds[ledNumber].brightness = DEFAULT_LED_BRIGHTNESS;
+		leds[ledNumber].red = red;
+		leds[ledNumber].green = green;
+		leds[ledNumber].blue = blue;
+	}
+	
+	UpdateAPA102CLeds(leds);
 }
--- apa102c.h
+++ apa102c.h
@@ -4,7 +4,7 @@
 	#define APA102C_DEFINED
 	
 	#define DEFAULT_LED_BRIGHTNESS 5			// Helderheid van de RGB-LED bij opstarten. Maximum 31...
-	#define NUMBER_OF_APA102C_LEDS 3
+	#define NUMBER_OF_APA102C_LEDS 5
 	
 	typedef struct{
 		uint8_t brightness;
@@ -14,4 +14,6 @@
 	} APA102C;		
 	
 	void UpdateAPA102CLeds(APA102C led[]);
+	void ClearAPA102CLeds(APA102C leds[]);
+	void SetAPA102CLed(APA102C leds[], uint8_t ledNumber, uint8_t red, uint8_t green, uint8_t blue);
 #endif
